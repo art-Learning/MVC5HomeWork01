70ed100d8137edfd9b53ca9251f5ad6c7f32a7eb
art <partypeopleland@gmail.com>
2015-04-22 17:23:29 +0800

    加入View的前台顯示

--- MVC5HomeWork01/Controllers/HomeController.cs
+++ MVC5HomeWork01/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC5HomeWork01.Models;
 
 namespace MVC5HomeWork01.Controllers {
     public class HomeController : Controller {
         public ActionResult Index() {
-            return View();
+            客戶資料Entities db = new 客戶資料Entities();
+            var info = db.V_總覽;
+            return View(info.ToList());
         }
 
         public ActionResult About() {
