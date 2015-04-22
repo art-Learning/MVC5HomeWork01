43dee674b9a517aba57afb83249d990e05a00370
art <partypeopleland@gmail.com>
2015-04-22 18:29:10 +0800

    加入表單驗證：必填及正則表達式規則

--- MVC5HomeWork01/Models/客戶聯絡人.cs
+++ MVC5HomeWork01/Models/客戶聯絡人.cs
@@ -11,15 +11,24 @@ namespace MVC5HomeWork01.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class 客戶聯絡人
     {
         public int Id { get; set; }
         public int 客戶Id { get; set; }
+        [Required]
         public string 職稱 { get; set; }
+        [Required]
         public string 姓名 { get; set; }
+        [Required]
+        [RegularExpression(@"^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})*$")]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{4}-\d{6}$")]
         public string 手機 { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{6,10}$")]
         public string 電話 { get; set; }
         public bool 是否已刪除 { get; set; }
     
--- MVC5HomeWork01/Models/客戶資料.cs
+++ MVC5HomeWork01/Models/客戶資料.cs
@@ -11,6 +11,7 @@ namespace MVC5HomeWork01.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class 客戶資料
     {
@@ -19,13 +20,23 @@ namespace MVC5HomeWork01.Models
             this.客戶銀行資訊 = new HashSet<客戶銀行資訊>();
             this.客戶聯絡人 = new HashSet<客戶聯絡人>();
         }
-    
+        
         public int Id { get; set; }
+        [Required]
         public string 客戶名稱 { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{8}$")]
         public string 統一編號 { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{6,10}$")]
         public string 電話 { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{6,10}$")]
         public string 傳真 { get; set; }
+        [Required]
         public string 地址 { get; set; }
+        [Required]
+        [RegularExpression(@"/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})*$/")]
         public string Email { get; set; }
         public bool 是否已刪除 { get; set; }
     
